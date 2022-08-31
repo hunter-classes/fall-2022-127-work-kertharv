4f492ace05f060e99cd435f845752ff22aff8af5
kertharv <91678853+kertharv@users.noreply.github.com>
2022-08-31 05:26:49 +0000

    deleted extra lines

--- chapter-2/clock.py
+++ chapter-2/clock.py
@@ -5,11 +5,8 @@ queue_time = int(input("Alarm: "))
 time_present = time_present + queue_time%24
 
 if time_present < 12:
-
-     print(str(time_present)+"am")
+  print(str(time_present)+"am")
 
 else:
-
-     time_present = time_present - 12
-
-     print(str(time_present)+"pm")
+  time_present = time_present - 12
+  print(str(time_present)+"pm")
