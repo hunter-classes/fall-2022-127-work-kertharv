ac6424f310540c00cf02c6860c583019bca69c52
kertharv <91678853+kertharv@users.noreply.github.com>
2022-09-01 13:19:18 +0000

    two turtles

--- code/turtles/t1.py
+++ code/turtles/t1.py
@@ -3,11 +3,31 @@ import turtle
 wn = turtle.Screen()
 
 crush = turtle.Turtle()
+
+#draw a square
 crush.forward(50)
-crush.left(30)
-crush.forward(130)
-crush.right(50)
-crush.forward(70)
+crush.right(90)
+crush.forward(50)
+crush.right(90)
+crush.forward(50)
+crush.right(90)
+crush.forward(50)
+crush.right(90)
+
+#create a second turtle
+squirt = turtle.Turtle()
+#into the variable
+#and make squirt draw a triangle
+squirt.up()
+squirt.goto(100, 100)
+squirt.down()
+squirt.color("red")
+squirt.left(120)
+squirt.forward(80)
+squirt.left(120)
+squirt.forward(80)
+squirt.left(120)   
+squirt.forward(80)
 
 wn.exitonclick()
 wn.mainloop()
