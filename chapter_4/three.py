52b4a35626f602130b92b0b825b79c3fcb7aa63b
kertharv <91678853+kertharv@users.noreply.github.com>
2022-09-06 00:57:08 +0000

    Exercise 7 code added

--- chapter_4/three.py
+++ chapter_4/three.py
@@ -1,3 +1,3 @@
-for amonth in ["January", "February", "March", "April", "May", "June", "July", "August", "September", "November", "December"]:
+for month in ["January", "February", "March", "April", "May", "June", "July", "August", "September", "November", "December"]:
     
-  print("One of the months of the year is", amonth)
+  print("One of the months of the year is", month)
