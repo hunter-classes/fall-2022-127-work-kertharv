0c208a63bab4938c6f96aeb97429ea9b2c542d72
kertharv <91678853+kertharv@users.noreply.github.com>
2022-09-08 13:03:47 +0000

    t1.py update

--- code/turtles/t1.py
+++ code/turtles/t1.py
@@ -1,36 +1,26 @@
 import turtle
 
-def sample_function():
-  print("This is a function")
-  print("It can be used multiple times")
-  
+
+#draw a square
+def square(t):
+  crush = turtle.Turtle()
+  for i in range (4):
+    t.forward(50)
+    t.right(90)
+
 wn = turtle.Screen()
 
 crush = turtle.Turtle()
 
-#draw a square
-for i in range (4):
-  crush.forward(50)
-  crush.right(90)
+square(crush)
 
-#create a second turtle
-#into the variable
 squirt = turtle.Turtle()
-#and make squirt draw a triangle
-squirt.up()
-squirt.goto(100, 100)
-squirt.down()
+squirt.penup()
+squirt.goto(100,100)
+squirt.pendown()
 squirt.color("red")
-
-for i in range(3):
-  squirt.left(120)
-  squirt.forward(80)
-
-sample_function()
-sample_function()
-sample_function()
+squirt.width(5)
+square(squirt)
 
 wn.exitonclick()
 wn.mainloop()
-
-a = 10 + 30
