1cbde049da4edd2e04500b758462efc473e923eb
kertharv <kertharv@gmail.com>
2022-10-03 13:44:09 -0500

    listlab.py added

---
+++ listlab.py
@@ -0,0 +1,9 @@
+#Chapter 10 Exercises 10, 11, 12
+countWords_list = ['red', 'white', 'black', 'green', 'blue']
+def countWords(countWords_list):
+    count = 0
+    if len(countWords_list) == 5:
+        count = count + 1
+
+result = countWords
+print(result)
