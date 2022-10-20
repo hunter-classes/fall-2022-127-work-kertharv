b0923164de09fc7f4b64deb6ddc06257ffc4dced
kertharv <kertharv@gmail.com>
2022-10-20 10:52:43 -0500

    added adj, bouns, story, and verbs as .dat files

---
+++ project-01-madlibs/madlibs.py
@@ -0,0 +1,5 @@
+# Extras
+# Extra #1: Instead of specifying the sentences or story to convert, 
+# write a story in a file and read it from your program. 
+# Make sure to include the file in your repo and that your program reads it correctly.
+# Extra #2: 
