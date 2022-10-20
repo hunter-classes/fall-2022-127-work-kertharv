9b3c14ededa9c413e782ee92cbd80c745ac7f563
kertharv <kertharv@gmail.com>
2022-10-20 11:13:39 -0500

    added content in txt/dat files

--- project-01-madlibs/madlibs.py
+++ project-01-madlibs/madlibs.py
@@ -2,4 +2,6 @@
 # Extra #1: Instead of specifying the sentences or story to convert, 
 # write a story in a file and read it from your program. 
 # Make sure to include the file in your repo and that your program reads it correctly.
-# Extra #2: 
+# Extra #2: Pay attention to letter case. That is, if you replace a word at the beginning of a sentence, 
+# it should be capitalized, otherwise, lowercase. 
+# This is except in the case of proper nouns which should always be capitalized.
