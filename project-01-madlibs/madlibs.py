7b933da7f596c9ecb0cb248913d287d3769abcff
kertharv <kertharv@gmail.com>
2022-10-22 14:37:12 -0500

    edited madlibs.py

--- project-01-madlibs/madlibs.py
+++ project-01-madlibs/madlibs.py
@@ -13,18 +13,22 @@ story = open("/home/kerth/fall-2022-127-work-kertharv/fall-2022-127-work-kerthar
 adj = open("/home/kerth/fall-2022-127-work-kertharv/fall-2022-127-work-kertharv/project-01-madlibs/adj.dat")
 nouns = open("/home/kerth/fall-2022-127-work-kertharv/fall-2022-127-work-kertharv/project-01-madlibs/nouns.dat")
 verbs = open("/home/kerth/fall-2022-127-work-kertharv/fall-2022-127-work-kertharv/project-01-madlibs/verbs.dat")
+hero = open("/home/kerth/fall-2022-127-work-kertharv/fall-2022-127-work-kertharv/project-01-madlibs/hero.dat")
 
 # Reads data previously written to a file.
 storyread = story.read()
 adjread = adj.read()
 nounsread = nouns.read()
 verbsread = verbs.read()
+heroread = hero.read()
 
 # Separate the words into strings.
 adjlist = adjread.split()
 nounslist = nounsread.split()
 verbslist = verbsread.split()
+herolist = heroread.split()
 
+# Calling these functions for madlibs function.
 def random_adj():
     return adjlist[random.randrange(len(adjlist))]
 
@@ -42,24 +46,29 @@ def madlibs(story):
     tracker = 0
     # Keeps track if a name is established yet and what index it is.            
     tracker_name = -1                
-    for i in lol:
-        # Adjectives code                              
-        if i == "<NOUN>":
-            # If this is the first time name is used.        
+    for p in lol:
+        # HERO code                              
+        if p == "<HERO>":
+            # If this is the first time hero is used.        
             if tracker_name == -1:      
-                # Calls premade random function at index of <NOUN> (repeated)              
+                # Calls premade random function at index of <hero> (repeated)              
                 lol[tracker] = random_noun()        
                 tracker_name = tracker             
             # Else, use the name from the index of first use. 
             else:                                 
                 lol[tracker] = lol[tracker_name]
+        # Nouns code
+        elif p == "<NOUN>":                       
+            lol[tracker] = random_noun().lower()
+            if(tracker == 0 or lol[tracker-1].find('.')):
+                lol[tracker].capitalize()
         # Verbs code
-        elif i == "<VERB>":                       
+        elif p == "<VERB>":                       
             lol[tracker] = random_verb().lower()
             if(tracker == 0 or lol[tracker-1].find('.')):
                 lol[tracker].capitalize()
         # Adjectives code
-        elif i == "<ADJ>":                    
+        elif p == "<ADJ>":                    
             lol[tracker] = random_adj().lower()
             if(tracker == 0 or lol[tracker-1].find('.')):
                 lol[tracker].capitalize()
