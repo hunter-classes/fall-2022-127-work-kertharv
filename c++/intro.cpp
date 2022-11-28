f6098308e5af41329d6231cf7c8cea10062faa2c
kertharv <kertharv@gmail.com>
2022-11-28 09:37:22 -0600

    c++ intro class code

---
+++ c++/intro.cpp
@@ -0,0 +1,32 @@
+#include <iostream>
+
+// This is a single line comment.
+/*
+    This is a multi line 
+    comment
+
+    This is a multi line
+    comment
+    to compile:
+    g++ intro.cpp
+    or
+    g++ -o intro intro.cpp
+
+    to run:
+    ./a.out
+    or
+    ./intro.cpp
+  
+
+*/
+
+int main() // Here to end of line is a comment
+{
+    std::cout << "Hello World!" << std::endl;
+
+    return 0;
+}
+
+// In python,the code above to would be tantamoun to:
+// def main ():
+//      return 0
